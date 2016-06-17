09f5c41795c82752a8d307b6919bf27a4c6e1fd1
aclemus <allisonclemus@gmail.com>
2016-06-16 19:05:52 -0700

    Morse errors gone! :)

--- Main.cs
+++ Main.cs
@@ -8,121 +8,124 @@ namespace Main
 		{
 			Console.Write("Hello");
 		}
-	}
 
-	public char Translator(string MorseCode)
-	{
-
-		switch (MorseCode)
+		public static char Translator(string MorseCode)
 		{
-			case ".-":
-				return 'a';
-				break;
-			case "-...":
-				return 'b';
-				break;
-			case "-.-.":
-				return 'c';
-				break;
-			case "-..":
-				return 'd';
-				break;
-			case ".":
-				return 'e';
-				break;
-			case "..-.":
-				return 'f';
-				break;
-			case "--.":
-				return 'g';
-				break;
-			case "....":
-				return 'h';
-				break;
-			case "..":
-				return 'i';
-				break;
-			case ".---":
-				return 'j';
-				break;
-			case "-.-":
-				return 'k';
-				break;
-			case ".-..":
-				return 'l';
-				break;
-			case "--":
-				return 'm';
-				break;
-			case "-.":
-				return 'n';
-				break;
-			case "---":
-				return 'o';
-				break;
-			case ".--.":
-				return 'p';
-				break;
-			case "--.-":
-				return 'q';
-				break;
-			case ".-.":
-				return 'r';
-				break;
-			case "...":
-				return 's';
-				break;
-			case "-":
-				return 't';
-				break;
-			case "..-":
-				return 'u';
-				break;
-			case "...-":
-				return 'v';
-				break;
-			case ".--":
-				return 'w';
-				break;
-			case "-..-":
-				return 'x';
-				break;
-			case "-.--":
-				return 'y';
-				break;
-			case "--..":
-				return 'z';
-				break;
-			case ".----":
-				return '1';
-				break;
-			case "..---":
-				return '2';
-				break;   
-			case "...--":
-				return '3';
-				break;
-			case "....-":
-				return '4';
-				break;
-			case ".....":
-				return '5';
-				break;
-			case "-....":
-				return '6';
-				break;
-			case "--...":
-				return '7';
-				break;
-			case "---..":
-				return '8';
-				break;
-			case "----.":
-				return '9';
-				break;
-			case "-----":
-				return '0';
-				break;
+
+			switch (MorseCode)
+			{
+				case ".-":
+					return 'a';
+					//break;
+				case "-...":
+					return 'b';
+					//break;
+				case "-.-.":
+					return 'c';
+					//break;
+				case "-..":
+					return 'd';
+					//break;
+				case ".":
+					return 'e';
+					//break;
+				case "..-.":
+					return 'f';
+					//break;
+				case "--.":
+					return 'g';
+					//break;
+				case "....":
+					return 'h';
+					//break;
+				case "..":
+					return 'i';
+					//break;
+				case ".---":
+					return 'j';
+					//break;
+				case "-.-":
+					return 'k';
+					//break;
+				case ".-..":
+					return 'l';
+					//break;
+				case "--":
+					return 'm';
+					//break;
+				case "-.":
+					return 'n';
+					//break;
+				case "---":
+					return 'o';
+					//break;
+				case ".--.":
+					return 'p';
+					//break;
+				case "--.-":
+					return 'q';
+					//break;
+				case ".-.":
+					return 'r';
+					//break;
+				case "...":
+					return 's';
+					//break;
+				case "-":
+					return 't';
+					//break;
+				case "..-":
+					return 'u';
+					//break;
+				case "...-":
+					return 'v';
+					//break;
+				case ".--":
+					return 'w';
+					//break;
+				case "-..-":
+					return 'x';
+					//break;
+				case "-.--":
+					return 'y';
+					//break;
+				case "--..":
+					return 'z';
+					//break;
+				case ".----":
+					return '1';
+					//break;
+				case "..---":
+					return '2';
+					//break;   
+				case "...--":
+					return '3';
+					//break;
+				case "....-":
+					return '4';
+					//break;
+				case ".....":
+					return '5';
+					//break;
+				case "-....":
+					return '6';
+					//break;
+				case "--...":
+					return '7';
+					//break;
+				case "---..":
+					return '8';
+					//break;
+				case "----.":
+					return '9';
+					//break;
+				case "-----":
+					return '0';
+					//break;
+				default:
+					return ' ';
+			}
 		}
 	}
+
 }
---
+++ Page1.cs
@@ -0,0 +1,23 @@
+using System;
+using Main;
+
+namespace Page1
+{
+	class ClassName
+	{
+		public void Function1()
+		{
+			Console.WriteLine("This is a function");
+		}
+	}
+	class ClassName_Main
+	{
+		static void Main() 
+		{
+			Hello main = new Hello();
+			Console.WriteLine("Sup");
+			main();
+
+		}
+	}
+}
