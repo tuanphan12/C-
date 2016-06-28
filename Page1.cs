b727eda39042b0185cfe591d20b51eaf4d333da5
aclemus <allisonclemus@gmail.com>
2016-06-27 20:35:39 -0700

    Forgot to include these, don't remember what they do :P

--- Page1.cs
+++ Page1.cs
@@ -1,23 +1,16 @@
 using System;
-using Main;
 
 namespace Page1
 {
 	class ClassName
 	{
-		public void Function1()
-		{
-			Console.WriteLine("This is a function");
-		}
-	}
-	class ClassName_Main
-	{
 		static void Main() 
 		{
-			Hello main = new Hello();
-			Console.WriteLine("Sup");
-			main();
-
+			string rawData = "#2104967205729<LF>";
+			Console.WriteLine("Number of Digits: {0}", rawData[1])
+			var numDigits = Convert.ToDouble(rawData[1]);
+			Console.WriteLine(numDigits);
 		}
 	}
+	
 }
