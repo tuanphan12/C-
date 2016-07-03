5eb9dde1a74bb8847b3d3458e3eb5da0ab750f65
aclemus <allisonclemus@gmail.com>
2016-07-03 10:06:03 -0700

    Tuple list happeniiiiiiiiing

--- Speed.cs
+++ Speed.cs
@@ -4,6 +4,22 @@ using System.Collections.Generic;
 
 namespace Speed
 {
+	public static class CommonMethods 
+	{
+		public static List<Tuple<string,string>> DictionaryToTupleList(Dictionary<string,string[]> deck) 
+		{
+			List<Tuple<string,string>> cardList = new List<Tuple<string,string>>();	
+			foreach(string suit in deck.Keys) 
+			{
+				foreach(string card in deck[suit])
+				{
+					var thisTuple = Tuple.Create(suit, card);
+					cardList.Add(thisTuple);
+				}
+			}
+			return cardList;
+		}
+	}
 	public class Game 
 	{
 		// First card to match
@@ -17,15 +33,18 @@ namespace Speed
 		// All of the cards known to no longer in the game
 		private Dictionary<string,string[]> _deck;
 
+		private List<Tuple<string,string>> cardList;
+
 		public void InitDeck()
 		{
 			Dictionary<string, string[]> deck = new Dictionary<string, string[]>();
 			string[] cards = {"1","2","3","4","5","6","7","8","9","10","jack","queen","king", "ace"};
-			deck.Add("hearts", cards);
-			deck.Add("spades", cards);
-			deck.Add("clubs", cards);
-			deck.Add("diamonds", cards);
+			deck.Add("h", cards);
+			deck.Add("s", cards);
+			deck.Add("c", cards);
+			deck.Add("d", cards);
 			this._deck = deck;
+			this.cardList = CommonMethods.DictionaryToTupleList(deck);
 		}
 
 		public void SetUp()
@@ -35,25 +54,16 @@ namespace Speed
 			{
 				Console.WriteLine("{0}: {1}", suit, string.Join(", ",this._deck[suit]));
 			}
+			foreach (Tuple<string,string> card in this.cardList)
+			{
+				Console.WriteLine(card.ToString());
+			}
+			
 		}
 		//Methods to:
 		// - Initiate player 1
 		// - Initiate computer player
 		// - Replace card1 and card2
-
-	}
-
-	public class PlayingGame
-	{
-		
-		public static void Main() 
-		{
-			Console.WriteLine("Let's build speed!");
-			Game g = new Game();
-			g.SetUp();
-			
-		}
-
 
 	}
 
@@ -69,6 +79,18 @@ namespace Speed
 		// - Refill hand
 		// - See hand
 		// - Declare "have no matches" to get new cards
+
+	}
+
+	public class PlayGame
+	{		
+		public static void Main() 
+		{
+			Console.WriteLine("Let's build speed!");
+			Game g = new Game();
+			g.SetUp();
+			
+		}
 
 	}
 }
